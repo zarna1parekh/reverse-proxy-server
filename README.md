fb24a876bd860d62b54e443a172331653e5a6c3e
Ubuntu <zparekh@zp-00.z2xv2yd4arsuzhsfptwxdy2poc.xx.internal.cloudapp.net>
2017-10-31 20:32:08 +0000

    Docker-compose Changes

--- README.md
+++ README.md
@@ -3,15 +3,17 @@ File description:
 - code.py - The python code for reverse-proxy server
 - dockerfile-mysql - Dockerfile to build the MYSQL DB image with info database and stats table
 - dockerfile-python - Dockerfile to build Python Application image by running code.py
+- docker-compose.yml - Dockore compose file for container configuration
 - init-db.sql - MYSQL DB Configs on starting
 - mysql_env - MYSQL DB enviornment variables
 - requirements.txt - Packages to be installed in the Python Application Image
-- run.sh - To buil and start all the containers
-- test.sh - Sample test script against run
+- run.sh - To build and start all the containers. Scale python_app to 5. Changes need to be made in run.sh to change the scale number
+- test.sh - Sample test script against run.sh
 
 Assumptions regarding packages:
 
 - Docker package installed
+- Docker-compose package installed ofr version '2'
 - Dependencies to run mysql client using python (python-dev, libmysqlclient-dev)
 
 The mysql container runs on port 3306 on the container and is mapped to port 6000 on the host. The data directory will be mapped to /storage/docker/mysql-datadir
--- run.sh
+++ run.sh
@@ -1,19 +1,27 @@
-!#/bin/bash
+#!/bin/bash
 
-#Create docker network
-sudo docker network create thousand_eyes
+##Create docker network
+#sudo docker network create thousand_eyes
 
-#create mysql_image image
-sudo docker build -t mysql_image -f dockerfile-mysql .
+##create mysql_image image
+#sudo docker build -t mysql_image -f dockerfile-mysql .
 
 #create a volume directory on the host
 sudo mkdir -p /storage/docker/mysql-datadir
 
-#run the mysql DB docker
-sudo docker run --name "mysql_db" --env-file ./mysql_env -p 6000:3306 --network "thousand_eyes" --volume=/storage/docker/mysql-datadir:/var/lib/mysql -d mysql_image 
+##run the mysql DB docker
+#sudo docker run --name "mysql_db" --env-file ./mysql_env -p 6000:3306 --network "thousand_eyes" --volume=/storage/docker/mysql-datadir:/var/lib/mysql -d mysql_image 
 
-#create python_app image
-sudo docker build -t python_app_image -f dockerfile-python .
+##create python_app image
+#sudo docker build -t python_app_image -f dockerfile-python .
 
-#run the python application docker
-sudo docker run --name python_code -p 5000:5000 --network "thousand_eyes" -d python_app_image
+##run the python application docker
+#sudo docker run --name python_code -p 5000:5000 --network "thousand_eyes" -d python_app_image
+
+####Modified run.sh after docker-compose####
+
+#To start 1 mysql_db, 1 python_application, 1 load_balancer
+sudo docker-compose up --build -d
+
+#to scale the python_application to 5 applications
+sudo docker-compose scale python_code=5
